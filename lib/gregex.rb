e188830f20c16758a9805162e53ded0b636ec117
lichtr <robert.lichtensteiner@gmail.com>
2014-06-12 01:27:42 +0200

    First steps in parsing the greek regexp

--- lib/gregex.rb
+++ lib/gregex.rb
@@ -1,5 +1,20 @@
 require "gregex/version"
 
 module Gregex
-  # Your code goes here...
+  plain_letters = %w(α ε ι η ο υ ω β γ δ ζ θ κ λ μ ν ξ π ρ ῥ ῤ σ ς τ φ χ ψ)
+  MAP = { '\w' => plain_letters }
+  def self.new(regex)
+    resolved = parse(regex)
+    Regexp.new(resolved)
+  end
+
+  def self.parse(regex)
+    string = regex.source
+    MAP.each do |meta, resolves|
+      if string.match(meta)
+        string.gsub!(meta, Regexp.union(MAP[meta]).source)
+      end
+    end
+    string
+  end
 end
--- spec/gregex_spec.rb
+++ spec/gregex_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Gregex do
+  let(:plain_letters) { %w(α ε ι η ο υ ω β γ δ ζ θ κ λ μ ν ξ π ρ ῥ ῤ σ ς τ φ χ ψ) }
   it 'should have a version number' do
     Gregex::VERSION.should_not be_nil
   end
@@ -8,7 +9,10 @@ describe Gregex do
   describe ".new" do
     it 'with \w' do
       regex = Gregex.new(/\w/)
-      "ι".match(regex)
+      #require 'pry'; binding.pry;
+      plain_letters.each do |letter|
+        expect(regex).to match(letter)
+      end
     end
   end
 end
