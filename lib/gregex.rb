81738c7ea262189775f478d26efaa89dcd5055fa
lichtr <robert.lichtensteiner@gmail.com>
2014-06-12 20:54:42 +0200

    Add greek lower case letters in constants

--- lib/gregex.rb
+++ lib/gregex.rb
@@ -1,8 +1,51 @@
 require "gregex/version"
 
 module Gregex
-  plain_letters = %w(α ε ι η ο υ ω β γ δ ζ θ κ λ μ ν ξ π ρ ῥ ῤ σ ς τ φ χ ψ)
-  MAP = { '\w' => plain_letters }
+  PLAIN_VOWELS = %w(α ε ι η ο υ ω)
+  VOWELS_WITH_ACUTE = %w(ά έ ή ί ó ύ ώ)
+  VOWELS_WITH_GRAVE = %w(ὰ ὲ ὴ ì ò ὺ ὼ)
+  VOWELS_WITH_CIRCUMFLEX = %w(ᾶ ῆ ῖ ῦ ῶ)
+  VOWELS_WITH_IOTA = %w(ᾲ ᾳ ᾴ ᾷ ῂ ῃ ῄ ῇ ῲ ῳ ῴ ῷ)
+  CONSONANTS = %w(β γ δ ζ θ κ λ μ ν ξ π ρ ῥ ῤ σ ς τ φ χ ψ)
+  VOWELS = [PLAIN_VOWELS,
+            VOWELS_WITH_ACUTE,
+            VOWELS_WITH_GRAVE,
+            VOWELS_WITH_CIRCUMFLEX,
+            VOWELS_WITH_IOTA
+  ].flatten
+
+  SPIRITUS_LENIS = %w(ἀ ἐ ἠ ἰ ὀ ὐ ὠ)
+  SPIRITUS_LENIS_WITH_GRAVE = %w(ἂ ἒ ἲ ἢ ὂ ὒ ὢ)
+  SPIRITUS_LENIS_WITH_ACUTE = %w(ἄ ἔ ἴ ἤ ὄ ὔ ὤ)
+  SPIRITUS_LENIS_WITH_CIRCUMFLEX = %w(ἆ ἶ ἦ ὖ ὦ )
+
+  SPIRITUS_ASPER = %w(ἁ ἑ ἡ ἱ ὁ ὑ ὡ)
+  SPIRITUS_ASPER_WITH_GRAVE = %w(ἃ ἣ ἓ ἳ ὃ ὓ ὣ)
+  SPIRITUS_ASPER_WITH_ACUTE = %w(ἅ ἥ ἕ ἵ ὅ ὕ ὥ)
+  SPIRITUS_ASPER_WITH_CIRCUMFLEX = %w(ἇ ἷ ἧ ὗ ὧ)
+
+  SPIRITUS_WITH_IOTA = %w(ᾀ ᾁ ᾂ ᾃ ᾄ ᾅ ᾆ ᾇ ᾐ ᾑ ᾒ ᾓ ᾔ ᾕ ᾖ ᾗ ᾠ ᾡ ᾢ ᾣ ᾤ ᾥ ᾦ ᾧ)
+
+  VOWELS_WITH_SPIRITUS = [
+    SPIRITUS_LENIS,
+    SPIRITUS_LENIS_WITH_ACUTE,
+    SPIRITUS_LENIS_WITH_GRAVE,
+    SPIRITUS_LENIS_WITH_CIRCUMFLEX,
+    SPIRITUS_ASPER,
+    SPIRITUS_ASPER_WITH_ACUTE,
+    SPIRITUS_ASPER_WITH_GRAVE,
+    SPIRITUS_ASPER_WITH_CIRCUMFLEX,
+    SPIRITUS_WITH_IOTA
+  ].flatten
+
+  ALL = [
+    VOWELS,
+    CONSONANTS,
+    VOWELS_WITH_SPIRITUS
+  ].flatten
+
+  MAP = { '\w' => ALL }
+
   def self.new(regex)
     resolved = parse(regex)
     Regexp.new(resolved)
@@ -12,6 +55,7 @@ module Gregex
     string = regex.source
     MAP.each do |meta, resolves|
       if string.match(meta)
+      #require 'pry'; binding.pry;
         string.gsub!(meta, Regexp.union(MAP[meta]).source)
       end
     end
--- spec/gregex_spec.rb
+++ spec/gregex_spec.rb
@@ -1,17 +1,33 @@
 require 'spec_helper'
 
 describe Gregex do
-  let(:plain_letters) { %w(α ε ι η ο υ ω β γ δ ζ θ κ λ μ ν ξ π ρ ῥ ῤ σ ς τ φ χ ψ) }
+  all_downcase_letters = %w(α ε ι η ο υ ω
+        β γ δ ζ θ κ λ μ ν ξ π ρ ῥ ῤ σ ς τ φ χ ψ
+        ά έ ή ί ó ύ ώ
+        ὰ ὲ ὴ ì ò ὺ ὼ
+        ᾶ ῆ ῖ ῦ ῶ
+        ἂ ἒ ἲ ἢ ὂ ὒ ὢ
+        ᾲ ᾳ ᾴ ᾷ ῂ ῃ ῄ ῇ ῲ ῳ ῴ ῷ
+        ἀ ἐ ἠ ἰ ὀ ὐ ὠ
+        ἄ ἔ ἴ ἤ ὄ ὔ ὤ
+        ἆ ἶ ἦ ὖ ὦ
+        ἁ ἑ ἡ ἱ ὁ ὑ ὡ
+        ἇ ἷ ἧ ὗ ὧ
+        ἃ ἣ ἓ ἳ ὃ ὓ ὣ
+        ἅ ἥ ἕ ἵ ὅ ὕ ὥ
+        ᾀ ᾁ ᾂ ᾃ ᾄ ᾅ ᾆ ᾇ ᾐ ᾑ ᾒ ᾓ ᾔ ᾕ ᾖ ᾗ ᾠ ᾡ ᾢ ᾣ ᾤ ᾥ ᾦ ᾧ )
+
   it 'should have a version number' do
     Gregex::VERSION.should_not be_nil
   end
 
   describe ".new" do
-    it 'with \w' do
+    context "with \w" do
       regex = Gregex.new(/\w/)
-      #require 'pry'; binding.pry;
-      plain_letters.each do |letter|
-        expect(regex).to match(letter)
+      all_downcase_letters.each do |letter|
+        it "matches #{letter}" do
+          expect(regex).to match(letter)
+        end
       end
     end
   end
