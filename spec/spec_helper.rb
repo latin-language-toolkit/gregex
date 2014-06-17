a1c3a8bbe63e6c4166eb5e039bcf576cbf11f4ae
lichtr <robert.lichtensteiner@gmail.com>
2014-06-18 00:04:24 +0200

    Configure specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,23 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'simplecov'
+require 'coveralls'
+
+Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
 require 'gregex'
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+end
+
