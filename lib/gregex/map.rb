751a4f919f9394af22bea4333ca19cb6b11933cd
lichtr <robert.lichtensteiner@gmail.com>
2014-08-05 22:41:31 +0200

    Make first test use of greek string gem

--- lib/gregex/map.rb
+++ lib/gregex/map.rb
@@ -1,14 +1,19 @@
+require 'greek_string'
 module Gregex
   class Map
 
     MAP = { '\w' => Gregex::Constants::ALL,
-            '[α-ω]' => Gregex::Constants::PLAIN_VOWELS,
+            '[α-ω]' => gs.select_by_types("vowel", "plain"),
             '[^α-ω]' => Gregex::Constants::CONSONANTS,
             '[ά-ώ]' => Gregex::Constants::VOWELS_WITH_ACUTE,
             '[ὰ-ὼ]' => Gregex::Constants::VOWELS_WITH_GRAVE,
             '[ᾶ-ῶ]' => Gregex::Constants::VOWELS_WITH_CIRCUMFLEX,
             '[β-ψ]' => Gregex::Constants::CONSONANTS,
     }
+
+    def self.gs
+      GreekString.new.selection
+    end
 
     attr_reader :map
 
