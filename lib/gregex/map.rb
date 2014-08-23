870829354544db05dd76b6039b27cae19d6556a9
lichtr <robert.lichtensteiner@gmail.com>
2014-08-23 17:33:54 +0200

    Add further vowels for options

--- lib/gregex/map.rb
+++ lib/gregex/map.rb
@@ -25,7 +25,10 @@ module Gregex
     def map
       if @opts.check_options("c")
         MAP['[α-ω]'] = Gregex::Constants::VOWELS + Gregex::Constants::VOWELS_WITH_SPIRITUS
+        MAP['α'] = Gregex::Map.gs.select_by_letter("Alpha").to_s
         MAP['ε'] = Gregex::Map.gs.select_by_letter("Epsilon").to_s
+        MAP['η'] = Gregex::Map.gs.select_by_letter("Eta").to_s
+        MAP['ι'] = Gregex::Map.gs.select_by_letter("Iota").to_s
         MAP['υ'] = Gregex::Map.gs.select_by_letter("Ypsilon").to_s
       end
       MAP
