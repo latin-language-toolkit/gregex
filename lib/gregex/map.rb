a1aed992f1160c3e3f8b2548bf7109e053532e53
lichtr <robert.lichtensteiner@gmail.com>
2014-08-26 23:31:17 +0200

    Refactor Gregex::Map

--- lib/gregex/map.rb
+++ lib/gregex/map.rb
@@ -11,25 +11,34 @@ class Gregex
       @map = map
     end
 
-    def regex_map
-      { '\w' => Gregex::Constants::ALL,
-        '[α-ω]' => @gs.selection.select_by_types("vowel", "plain"),
-        '[^α-ω]' => Gregex::Constants::CONSONANTS,
-        '[ά-ώ]' => Gregex::Constants::VOWELS_WITH_ACUTE,
-        '[ὰ-ὼ]' => Gregex::Constants::VOWELS_WITH_GRAVE,
-        '[ᾶ-ῶ]' => Gregex::Constants::VOWELS_WITH_CIRCUMFLEX,
-        '[β-ψ]' => Gregex::Constants::CONSONANTS,
-      }
-    end
-
     def map
       if @opts.check_options("c")
-        @regex_map['[α-ω]'] = Gregex::Constants::VOWELS + Gregex::Constants::VOWELS_WITH_SPIRITUS
+        @regex_map['[α-ω]'] = select(:vowel)
+
         Gregex::Constants::VOWEL_NAMES.each do |str, name|
           @regex_map[str] = @gs.selection.select_by_letter(name).to_s
         end
+
       end
       @regex_map
+    end
+
+    private
+
+    def regex_map
+      { '\w' => @gs.all.to_s,
+        '[α-ω]' => select(:vowel, :plain),
+        '[^α-ω]' => select(:consonant),
+        '[ά-ώ]' => select(:only_acute),
+        '[ὰ-ὼ]' => select(:only_grave),
+        '[ᾶ-ῶ]' => select(:only_circumflex),
+        '[β-ψ]' => select(:consonant)
+      }
+    end
+
+    def select(*args)
+      meth = args.length > 1 ? "types" : "type"
+      @gs.selection.send("select_by_#{meth}", *args).to_s
     end
   end
 end
